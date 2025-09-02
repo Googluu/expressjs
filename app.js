4851529544fd3d54cdbf50a8c40970d21180f9ab
Googluu <sneyder.h.rodriguez@gmail.com>
2025-09-02 15:38:31 -0500

    variables de entorno para local

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+require('dotenv').config()
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('hello world!');
+  res.send(`
+    <h1>Curso de expressjs</h1>
+    <p>con el profe oscar de platzi</p>
+    <p>Corriendo en el puerto: ${PORT}</p>
+    `);
 });
 
 app.listen(PORT, () => {
-    console.log('aplicacion esta corriendo!');
+  console.log(`Aplicacion esta corriendo en http://localhost:${PORT}`);
 });
