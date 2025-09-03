8ada22a62db524c2772996ff03fe5ae68a8e25c2
Googluu <sneyder.h.rodriguez@gmail.com>
2025-09-03 14:58:18 -0500

    json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 // require('dotenv').config()
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +14,22 @@ app.get('/', (req, res) => {
     <p>Corriendo en el puerto: ${PORT}</p>
     `);
 });
+
+app.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(`usuario con id: ${id}`);
+});
+
+
+app.get('/shearer', (req, res) => {
+  const { terms = 'no hay termino' } = req.query;
+  const { category = 'no hay categoria' } = req.query;
+  res.send(`
+    <h1>Buscador de shearer</h1>
+    <p>Buscando por: ${terms}</p>
+    <p>Categoria: ${category}</p>
+  `);
+})
 
 app.listen(PORT, () => {
   console.log(`Aplicacion esta corriendo en http://localhost:${PORT}`);
