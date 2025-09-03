cc72974c27740c0a25397afc9fe39928a286492c
Googluu <sneyder.h.rodriguez@gmail.com>
2025-09-03 15:10:43 -0500

    json

--- app.js
+++ app.js
@@ -31,6 +31,32 @@ app.get('/shearer', (req, res) => {
   `);
 })
 
+app.post('/form', (req, res) => {
+  const {name = 'no hay nombre', email = 'no hay email', age = 'no hay edad'} = req.body;
+  
+  res.json({
+    message: 'Formulario recibido',
+    name,
+    email,
+    age
+  });
+});
+
+app.post('/data', (req, res) => {
+  const data = req.body;
+
+  if (!data || Object.keys(data).length === 0) {
+    return res.status(400).json({
+      message: 'No se recibieron datos'
+    });
+  }
+
+  res.status(200).json({
+    message: 'Datos recibidos',
+    data
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Aplicacion esta corriendo en http://localhost:${PORT}`);
 });
